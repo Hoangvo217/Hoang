92f6217f1918b987aaac035c4a374d9e1de58a2a
Hoangvo217 <118655390+Hoangvo217@users.noreply.github.com>
2022-12-11 14:09:34 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+function CheckBrowser(){
+    if('localStorage'in window && window['localStorage'] !==null) {
+        return true;
+    } else{ return false;
+    }
+}
+function doShowALL() {
+    if (CheckBrowser()){
+        var key="";
+        var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+        var i=0;
+        for (i=0; i<= localStorage.length-1; i++){
+            key = localStorage.key(i);
+            list += "<tr><td>" +key+"</td>\n<td>"
+                + localStorage.getItem(key)+"</td></tr>\n";
+        }
+        if(list == "<tr><th>Item</th><th>Value</th></tr>\n"){
+            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+        }
+        document.getElementById('list').innerHTML=list
+    } else{
+        alert('cannot save shopping list as your browser does not support HTML 5');
+    }
+}
+localStorage.setItem('propertyName','value');
+localStorage.getItem('propertyName');
+localStorage.removeItem('propertyName');
+function saveItem() {
+    var name=document.forms.ShoppingList.name.value;
+    var data=document.forms.ShoppingList.data.value;
+    localStorage.setItem(name,data);
+    doShowALL();
+}
+function ModifyItem() {
+    var name1=document.forms.ShoppingList.name.value;
+    var data1=document.forms.ShoppingList.data.value;
+    if(localStorage.getItem(name1) !=null)
+    {
+        localStorage.setItem(name1,data1);
+        document.forms.ShoppingList.data.value=localStorage.getItem(name1);
+
+    }
+    doShowALL();
+}
+function RemoveItem(){
+    var name=document.forms.ShoppingList.name.value;
+    document.forms.ShoppingList.data.value=localStorage.removeItem(name);
+    doShowALL();
+}
+function ClearALL() {
+    localStorage.clear();
+    doShowALL();
+}
